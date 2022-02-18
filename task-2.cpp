f6f35d214d85ca0be016a22b2adbd0f7d2633cee
TanyaMamontova <99840067+TanyaMamontova@users.noreply.github.com>
2022-02-18 20:19:54 +0300

    Update task-2.cpp

--- task-2.cpp
+++ task-2.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-
 using namespace std;
-float ploshad(float dlina_1, float dlina_2)
+
+float ploshad(float dlina_1, float dlina_2) 
 {
-cout << dlina_1 * dlina_2 << endl;
-return 0;
+	return dlina_1 * dlina_2;
 }
 
 int main()
 
 {
-float dlina_1, dlina_2;
-cin >> dlina_1;
-cin >> dlina_2;
-ploshad(dlina_1, dlina_2);
-return 0;
+	float dlina_1, dlina_2;
+	cin >> dlina_1;
+	cin >> dlina_2;
+	cout << ploshad(dlina_1, dlina_2) << endl;
 }
+
