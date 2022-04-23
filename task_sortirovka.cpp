b37fff1dbd024e9063175eff91f805c897774478
TanyaMamontova <99840067+TanyaMamontova@users.noreply.github.com>
2022-04-23 13:57:16 +0300

    Update task_sortirovka.cpp

--- task_sortirovka.cpp
+++ task_sortirovka.cpp
@@ -2,27 +2,24 @@
 
 using namespace std;
 
-void sortirovka(int size, int* sarr){
+void sortirovka(int size, int* sarr) { //Функция для сортировки массива пузырьком по возрастанию
  int schet = 0;
  int max;
- //max = sarr[0];
  while (schet < size){
-     
-     for (int j = 1; j < size-1; j++){
-         if (sarr[j] > sarr[j+1]){
-             max = sarr[j];
-             sarr[j] = sarr[j+1];
-             sarr[j+1] = max;
-            } 
+     for(int i = 0; i < size; i++){
+            for (int j = 0; j < i; j++){
+                 if (sarr[j] > sarr[j+1]){
+                 max = sarr[j];
+                 sarr[j] = sarr[j+1];
+                 sarr[j+1] = max;
+                } 
+            }
         }
-
      schet++;
-   }
+    }
 }
     
- 
-  int main()
-{
+  int main() {
   int n;
   cout << "Введите количество элементов массива: ";
   cin >> n;
@@ -30,7 +27,7 @@ void sortirovka(int size, int* sarr){
   sarr = new int[n];
   
   cout << "Введите элементы массива: " << endl;
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++) {
        cin >> sarr[i];
     }
   sortirovka(n, sarr);
