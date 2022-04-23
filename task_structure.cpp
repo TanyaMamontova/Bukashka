294656b5a1ff125284c400b505456fa205358842
TanyaMamontova <99840067+TanyaMamontova@users.noreply.github.com>
2022-04-23 11:32:57 +0300

    Update task_structure.cpp

--- task_structure.cpp
+++ task_structure.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 struct drob {
@@ -10,15 +9,14 @@ struct drob {
 void sokr ( int& x, int& y) //Функция, для нахождения НОД двух чисел
 {
 int a, b, c;
-a = x;
-b = y;
-    do
+a = abs(x);
+b = abs(y);
+    while (b > 0)
     {
         c = a % b;
         a = b;
         b = c;
     }
-    while (b > 0);
     x /= a;
     y /= a;
 }
@@ -26,11 +24,14 @@ b = y;
 int main()
 {
   drob X;
-  cout << "Введите числитель дроби: ";
+    cout << "Введите числитель дроби: ";
     cin >> X.chisl;
     cout << "Введите знаменатель дроби: ";
     cin >> X.znamen;
     sokr (X.chisl, X.znamen);
+    if (X.chisl < 0 && X.znamen < 0)
+    cout << "Дробь после сокращения: " << abs(X.chisl) << "/" << abs(X.znamen);
+    else
     cout << "Дробь после сокращения: " << X.chisl << "/" << X.znamen; 
     cout << endl;
 }
