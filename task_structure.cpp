19401da45b0b49a70051ed68a2bee7a65d62854b
TanyaMamontova <99840067+TanyaMamontova@users.noreply.github.com>
2022-04-26 15:35:39 +0300

    Update task_structure.cpp
    Исправила задачу со структурой

--- task_structure.cpp
+++ task_structure.cpp
@@ -7,17 +7,17 @@ struct drob {
     int znamen;
 };
 
-void sokr (int& x, int& y) { //Функция, для нахождения НОД двух чисел
+void sokr (drob& foo) { //Функция, для нахождения НОД двух чисел
 int a, b, c;
-a = abs(x);
-b = abs(y);
+a = abs(foo.chisl);
+b = abs(foo.znamen);
     while (b > 0) {
         c = a % b;
         a = b;
         b = c;
     }
-x /= a;
-y /= a;
+foo.chisl /= a;
+foo.znamen /= a;
 }
  
 int main() {
@@ -26,13 +26,13 @@ int main() {
     cin >> X.chisl;
     cout << "Введите знаменатель дроби: ";
     cin >> X.znamen;
-    sokr (X.chisl, X.znamen);
-    if (X.chisl == 0 || X.znamen == 0) {
+    sokr (X);
+    if (X.chisl == 0  X.znamen == 0) {
     cout << "Некорректная дробь!";
 }
 else {
     cout << "Дробь после сокращения: ";
-    if (X.chisl > 0 && X.znamen < 0 || X.chisl < 0 && X.znamen > 0) {
+    if (X.chisl > 0 && X.znamen < 0  X.chisl < 0 && X.znamen > 0) {
         cout << "-";
     }
     cout << abs(X.chisl) << "/" << abs(X.znamen);
